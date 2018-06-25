2d5b5c6e306ceb7b48d158a77994304c3ad84c50
ayulestari10 <lestariayu669@gmail.com>
2018-06-25 10:20:49 +0700

    database2

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '52.187.182.63',
 	'username' => 'root',
 	'password' => '4kuGanteng',
 	'database' => 'dokumentasita',
