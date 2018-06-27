c0ed3e714af4809dff85b1f4ac5af84ea7a56d9b
ayulestari10 <lestariayu669@gmail.com>
2018-06-27 22:21:21 +0700

    style

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -39,7 +39,7 @@
 			<div class="vertical-align-middle">
 				<div class="auth-box ">
 					<div class="left">
-						<div class="content">
+						<div class="content" style="width: 95% !important;">
 							<div class="header">
 								<!-- <div class="logo text-center"><img src="<?= base_url('assets/login/') ?>assets/img/logo-dark.png" alt="Klorofil Logo"></div> -->
 								<h4><strong>Login Dokumentasi TA</strong></h4>
