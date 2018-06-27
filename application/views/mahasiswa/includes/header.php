69f9b2a8c7ebb8a390d3561e56198e963d86685a
sarirahman <trikurniasari05@gmail.com>
2018-06-27 22:59:08 +0700

    sweetlaert coba

--- application/views/mahasiswa/includes/header.php
+++ application/views/mahasiswa/includes/header.php
@@ -37,8 +37,8 @@
     <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support -->
     <!-- <script src="<?= base_url('assets') ?>/sweetalert2/dist/sweetalert2.all.min.js"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.4.1/sweetalert.css" rel="stylesheet" />
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.4.1/sweetalert.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css" rel="stylesheet" />
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
     <!-- <script src="<?= base_url('assets') ?>/sweetalert2/dist/sweetalert2.min.js"></script>
     <link rel="stylesheet" href="<?= base_url('assets') ?>/sweetalert2/dist/sweetalert2.min.css"> -->
   </head>
