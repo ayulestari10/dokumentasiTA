25978b4eeeaa26c7a0a532cd248602b7df230a23
sarirahman <trikurniasari05@gmail.com>
2018-06-28 06:47:20 +0700

    saa+

--- application/views/mahasiswa/includes/header.php
+++ application/views/mahasiswa/includes/header.php
@@ -40,12 +40,17 @@
     <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support -->
     <!-- <script src="https://cdn.jsdelivr.net/npm/promise-polyfill"></script>
      -->
-    <script src="<?= base_url('assets') ?>/sweetalert2/dist/sweetalert2.all.min.js"></script>
+    <!-- <script src="<?= base_url('assets') ?>/sweetalert2/dist/sweetalert2.all.min.js"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css" rel="stylesheet" />
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
-    <script src="<?= base_url('assets') ?>/sweetalert2/dist/sweetalert2.min.js"></script>
-    <link rel="stylesheet" href="<?= base_url('assets') ?>/sweetalert2/dist/sweetalert2.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.24.3/sweetalert2.all.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.24.3/sweetalert2.all.min.js"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.24.3/sweetalert2.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.24.3/sweetalert2.css"> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.24.3/sweetalert2.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.24.3/sweetalert2.min.css">
+    
+    <!-- <script src="<?= base_url('assets') ?>/sweetalert2/dist/sweetalert2.min.js"></script>
+    <link rel="stylesheet" href="<?= base_url('assets') ?>/sweetalert2/dist/sweetalert2.min.css"> -->
     <!-- Automatically provides/replaces `Promise` if missing or broken. -->
 <!--     <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.js"></script>  -->
