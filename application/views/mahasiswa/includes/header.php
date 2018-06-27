b3d271b79581459a55cc5c3c9477882c275b7569
sarirahman <trikurniasari05@gmail.com>
2018-06-28 06:17:03 +0700

    sa

--- application/views/mahasiswa/includes/header.php
+++ application/views/mahasiswa/includes/header.php
@@ -34,17 +34,21 @@
     <link href="<?= base_url('assets') ?>/vendors/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
 
     <link rel="icon" type="image/png" sizes="96x96" href="<?= base_url('assets/foto/logo.png') ?>">
-    <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support -->
+    
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
 
-    <!-- <script src="<?= base_url('assets') ?>/sweetalert2/dist/sweetalert2.all.min.js"></script> -->
+    <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support -->
+    <script src="https://cdn.jsdelivr.net/npm/promise-polyfill"></script>
+    
+    <script src="<?= base_url('assets') ?>/sweetalert2/dist/sweetalert2.all.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css" rel="stylesheet" />
    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
     <script src="<?= base_url('assets') ?>/sweetalert2/dist/sweetalert2.min.js"></script>
     <link rel="stylesheet" href="<?= base_url('assets') ?>/sweetalert2/dist/sweetalert2.min.css">
     <!-- Automatically provides/replaces `Promise` if missing or broken. -->
-    <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.js"></script> 
+<!--     <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.js"></script>  -->
   </head>
 
   <body class="nav-md">
