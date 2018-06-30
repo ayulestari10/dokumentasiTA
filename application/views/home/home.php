6649d1ed7935efe5cdf061869bb5e093de244d03
sarirahman <trikurniasari05@gmail.com>
2018-06-30 10:10:34 +0700

    tulisan+

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -185,5 +185,14 @@
           });
         });
       });
+
+      $('#pagination-demo').twbsPagination({
+        totalPages: 35,
+        visiblePages: 7,
+        onPageClick: function (event, page) {
+            $('#page-content').text('Page ' + page);
+        }
+      });
+
     </script>
 
